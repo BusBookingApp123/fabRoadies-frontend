94b507c5e575af3ac89467e943b6f629b5c45aab
Sancharisaha18 <sancharisaha6@gmail.com>
2021-06-11 00:11:51 +0530

    Update app.module.ts

--- FabRoadies/src/app/app.module.ts
+++ FabRoadies/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowseComponent } from './browse/browse.component';
 import { LoginComponent } from './login/login.component';
 import{HttpClientModule} from'@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import{HttpClientModule} from'@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    FormsModule
     AppRoutingModule,
     HttpClientModule
   ],
