764e2b310a8b1ac3f3dd290d13808f3299b58ade
BusBookingApp123 <aninditasengupta2@gmail.com>
2021-06-11 09:43:19 +0530

    Update browse.component.ts

--- FabRoadies/src/app/browse/browse.component.ts
+++ FabRoadies/src/app/browse/browse.component.ts
@@ -8,9 +8,22 @@ import { Router } from '@angular/router';
 })
 export class BrowseComponent implements OnInit {
 
+  public name:string='';
+  public email:string='';
+  public phnNo:number=0;
+  public feedBack:string='';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  feedback(){
+    if(this.name&&this.email&&this.feedBack!=null){
+      alert('Thanks for the Feedback!')
+    }
+    console.log(this.name+"has "+this.email+"and "+this.phnNo);
+    console.log(this.feedBack);
   }
 
 }
