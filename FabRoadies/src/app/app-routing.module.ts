a5e5828bd03a7e372f82b09d958cf6e233c5cedf
BusBookingApp123 <aninditasengupta2@gmail.com>
2021-06-11 21:32:38 +0530

    Update app-routing.module.ts

--- FabRoadies/src/app/app-routing.module.ts
+++ FabRoadies/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminBrowseComponent } from './admin-browse/admin-browse.component';
 import { BrowseComponent } from './browse/browse.component';
 import { LoginComponent } from './login/login.component';
 
@@ -7,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {path:'',component:BrowseComponent},
   {path:'login',component:LoginComponent},
+  {path:'admin-browse',component:AdminBrowseComponent},
   {path:'**',redirectTo:'',pathMatch:'full'}
 ];
 
