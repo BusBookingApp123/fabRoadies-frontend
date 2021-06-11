4e83e81c55fe5ab6da31e8d2ec49684ddf0c877c
BusBookingApp123 <aninditasengupta2@gmail.com>
2021-06-11 14:39:02 +0530

    Create admin.service.ts

---
+++ FabRoadies/src/app/services/admin.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BusAdmin } from 'src/BusAdmin.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  busList:BusAdmin[]=[{
+    "busNo":123,
+    "agency":"Demo Agency",
+    "busType":"Demo Type",
+    "depCity":"Demo Cityn",
+    "dateOfDep":"00/00/0000",
+    "depTime":1234
+  }];
+
+  constructor() { }
+
+  // addBus(admin:BusAdmin){
+  //   return this.busList.push(admin);
+  // }
+}
