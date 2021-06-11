1866a4f96d369375b844c82827011cbf8d7b9453
Sancharisaha18 <sancharisaha6@gmail.com>
2021-06-11 22:46:19 +0530

    Update signup.component.ts

--- FabRoadies/src/app/signup/signup.component.ts
+++ FabRoadies/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/user.model';
 
 @Component({
@@ -17,12 +18,7 @@ export class SignupComponent implements OnInit {
   }
 
   save() {
-    /**if(this.user.email=='admin@email.com'&&this.user.password=='admin123')
-      this.router.navigate(['admin']);
-    else if(this.user.email=='fabroadies@gmail.com' && this.user.password == 'fabroadies')
-      this.flag = true;
-    else
-      alert("Invalid user id");**/
+      alert("Your data is submitted successfully");
     console.log("Your data is submitted successfully" + this.user.name +":"+ this.user.phnNo +":"+ this.user.email +":"+ this.user.password);
   }
 
