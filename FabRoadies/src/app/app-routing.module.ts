23e1c7b6789e313535e66ad5c58570b49fae7a2c
Sancharisaha18 <sancharisaha6@gmail.com>
2021-06-11 22:45:58 +0530

    Update app-routing.module.ts

--- FabRoadies/src/app/app-routing.module.ts
+++ FabRoadies/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminBrowseComponent } from './admin-browse/admin-browse.component';
 import { BrowseComponent } from './browse/browse.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
   {path:'',component:BrowseComponent},
   {path:'login',component:LoginComponent},
   {path:'admin-browse',component:AdminBrowseComponent},
-  {path:'**',redirectTo:'',pathMatch:'full'}
+  {path:'**',redirectTo:'',pathMatch:'full'},
+  {path:'signup',component:SignupComponent}
 ];
 
 @NgModule({
