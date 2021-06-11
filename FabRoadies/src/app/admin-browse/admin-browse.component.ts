64a3f2a6596d66460987693fdf2e14a5f825ec4d
BusBookingApp123 <aninditasengupta2@gmail.com>
2021-06-11 21:32:32 +0530

    Create admin-browse.component.ts

---
+++ FabRoadies/src/app/admin-browse/admin-browse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-browse',
+  templateUrl: './admin-browse.component.html',
+  styleUrls: ['./admin-browse.component.css']
+})
+export class AdminBrowseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
