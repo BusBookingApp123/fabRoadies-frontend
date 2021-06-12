f84b9cd643ac0d0ab711d22aeb5b945dd297264f
BusBookingApp123 <aninditasengupta2@gmail.com>
2021-06-12 11:33:24 +0530

    Added the no-buses found page

--- FabRoadies/src/app/app.module.ts
+++ FabRoadies/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from'@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { BrowseComponent } from './browse/browse.component';
 import { LoginComponent } from './login/login.component';
-import{HttpClientModule} from'@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
 import { AdminBrowseComponent } from './admin-browse/admin-browse.component';
 import { AddBusComponent } from './add-bus/add-bus.component';
+import { NoBusesFoundComponent } from './no-buses-found/no-buses-found.component';
+
+
 
 @NgModule({
   declarations: [
@@ -19,13 +25,17 @@ import { AddBusComponent } from './add-bus/add-bus.component';
     SignupComponent,
     AdminBrowseComponent,
     AddBusComponent,
+    NoBusesFoundComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
