df74e21b43122bc04bc42f9ff24219433f36cbfb
BusBookingApp123 <aninditasengupta2@gmail.com>
2021-06-12 10:39:09 +0530

    Update BusAdmin.model.ts

--- FabRoadies/src/BusAdmin.model.ts
+++ FabRoadies/src/BusAdmin.model.ts
@@ -1,5 +1,7 @@
 export class BusAdmin{
     public busNo:number=0;
+    public source:string='';
+    public destination:string='';
     public agency:string='';
     public busType:string='';
     public depCity:string='';
