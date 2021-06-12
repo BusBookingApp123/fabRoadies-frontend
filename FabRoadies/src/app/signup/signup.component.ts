f679861a705c8507c17ec926d4a583892d330aad
Sancharisaha18 <sancharisaha6@gmail.com>
2021-06-12 11:01:31 +0530

    Update signup.component.ts

--- FabRoadies/src/app/signup/signup.component.ts
+++ FabRoadies/src/app/signup/signup.component.ts
@@ -17,7 +17,7 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  save() {
+  onSubmit() {
     console.log("Your data is submitted successfully" + this.user.name +":"+ this.user.phnNo +":"+ this.user.email +":"+ this.user.password);
     alert("Your data is submitted successfully");
   }
