4aa8141d655a163293e4e7f0aa5c3be34204651c
Sancharisaha18 <sancharisaha6@gmail.com>
2021-06-13 09:37:38 +0530

    Update BusAdmin.model.ts

--- FabRoadies/src/BusAdmin.model.ts
+++ FabRoadies/src/BusAdmin.model.ts
@@ -1,10 +1,10 @@
 export class BusAdmin{
-    public busNo:number=0;
-    public source:string='';
-    public destination:string='';
-    public agency:string='';
-    public busType:string='';
-    public depCity:string='';
-    public dateOfDep:string='';
-    public depTime:number=0;
+    public busNo : number = 0;
+    public source : string = '';
+    public destination : string = '';
+    public agency : string = '';
+    public busType : string = '';
+    public depCity : string = '';
+    public dateOfDep : string = '';
+    public depTime : number = 0;
 }
