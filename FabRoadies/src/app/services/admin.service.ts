be7ce50f26fc1479a6ff828f155cafedc3a4606e
Sancharisaha18 <sancharisaha6@gmail.com>
2021-06-14 23:59:35 +0530

    Update admin.service.ts

--- FabRoadies/src/app/services/admin.service.ts
+++ FabRoadies/src/app/services/admin.service.ts
@@ -4,53 +4,51 @@ import { Injectable } from '@angular/core';
 import { BusAdmin } from 'src/BusAdmin.model';
 import { HttpClient } from '@angular/common/http';
 import { delay } from 'rxjs/operators';
+import { User } from 'src/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  busList: BusAdmin[] = [{
-    "busNo": 124,
-    "source": "From Demo",
-    "destination": "To Demo2",
-    "agency": "Demo Agency",
-    "busType": "Demo Type",
-    "dateOfDep": "00/00/0000",
-    "depTime": 1234,
-    "noOfseats": 43,
-    "price": 250
-  }];
-  //[];
-  //baseUrl : string = "http://localhost:8080"
+  bus:BusAdmin[]=[];
+  baseUrl : string = "http://localhost:8080";
   
 
-  //constructor(private http : HttpClient) { }
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
 
   addAdminBus(addBus: BusAdmin) {
-    //this.http.post(this.baseUrl+"/add-bus",addBus).subscribe(data => data = addBus);
-    this.busList.push(addBus);
+    this.http.post(this.baseUrl+"/admin/addBus",addBus).subscribe(data => data = addBus);
+
   }
 
-  getAdminBusList(): BusAdmin[] {
-    //async getAdminBusList(): BusAdmin[] {
-    //return await this.http.get<BusAdmin[]>(this.baseUrl+"/busList").pipe(delay(100)).toPromise();
-    return this.busList;
+
+    async getAdminBusList() {
+
+   return await this.http.get<BusAdmin[]>(this.baseUrl+"/admin/showAllBuses").toPromise();
   }
 
-  findByBusNo(busNo:number){
-    //return this.http.get<Flight>(this.baseUrl+"/flight/"+code);
-    return this.busList.find(x=>x.busNo==busNo);
+  deleteBus(busno:string){
+  
+    this.http.delete(this.baseUrl + "/admin/deleteBus/" +busno).subscribe();
+
+
+     }
+
+  findByBusNo(busno:string){
+ 
+    return this.http.get<BusAdmin>(this.baseUrl+"/get/bus/"+busno);
+   // return this.busList.find(x=>x.busno==busno);
   }
 
-  deleteBus(busNo:number){
-    //deleteBus(code : number){
-      //this.http.delete(this.baseUrl + "/del/" +code).subscribe();
-    return this.busList.splice(busNo,1);
-  }
+ 
 
   updateBus(addBus : BusAdmin){
-    //this.http.put(this.baseUrl+"/edit", addBus).subscribe();
+    this.http.put(this.baseUrl+"/bus/edit", addBus).subscribe();
+  }
+  addUser(user:User)
+  {
+    this.http.post(this.baseUrl+"/add/2",user).subscribe();
   }
 }
