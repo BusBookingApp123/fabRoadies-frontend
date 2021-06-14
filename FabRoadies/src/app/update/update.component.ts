aee54d3aab3a844397ff550be996dd5473302e73
Sancharisaha18 <sancharisaha6@gmail.com>
2021-06-14 14:39:07 +0530

    Update update.component.ts

--- FabRoadies/src/app/update/update.component.ts
+++ FabRoadies/src/app/update/update.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute,Router } from '@angular/router';
 import { BusAdmin } from 'src/BusAdmin.model';
 import { AdminService } from '../services/admin.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-update',
@@ -10,22 +11,28 @@ import { AdminService } from '../services/admin.service';
 })
 export class UpdateComponent implements OnInit {
 
-  addBus:BusAdmin=new BusAdmin();
+  addBus : BusAdmin = new BusAdmin();
+
   source : string[] = ["Kolkata","Chennai","Pune","Mumbai","Bangalore","Delhi"];
+
   destination : string[] = ["Kolkata","Chennai","Pune","Mumbai","Bangalore","Delhi"]
+
   busType:string[]=["AC","Non-AC"]
 
-  constructor(private service:AdminService,private router:Router) { }
+  constructor(private service:AdminService, private router:Router, private aroute : ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    /*this.aroute.queryParams.pipe(filter(params => params.bno)).subscribe(params => {
+      var pbno = params.bno;
+      this.service.findByBusNo(pbno).subscribe(data => this.addBus = data);
+    })*/
   }
 
-  save(){
-    this.service.addAdminBus(this.addBus);
-    console.log("Saving...." + this.addBus.busNo);
-    alert("Your entry is saved successfully");
-    this.router.navigate(['admin_bus-list']);
-  }
+  update() {
+    this.service.updateBus(this.addBus);
+    this.router.navigate(['list']).then(()=>location.reload());
+  } 
 
   //logout function
   logout(){
