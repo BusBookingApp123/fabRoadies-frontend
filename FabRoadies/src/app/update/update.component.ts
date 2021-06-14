19433057ae03c5a5079dc690d5aab3752dd37348
BusBookingApp123 <aninditasengupta2@gmail.com>
2021-06-14 11:19:53 +0530

    Update update.component.ts

--- FabRoadies/src/app/update/update.component.ts
+++ FabRoadies/src/app/update/update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BusAdmin } from 'src/BusAdmin.model';
+import { AdminService } from '../services/admin.service';
 
 @Component({
   selector: 'app-update',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
+  addBus:BusAdmin=new BusAdmin();
+  source : string[] = ["Kolkata","Chennai","Pune","Mumbai","Bangalore","Delhi"];
+  destination : string[] = ["Kolkata","Chennai","Pune","Mumbai","Bangalore","Delhi"]
+  busType:string[]=["AC","Non-AC"]
+
+  constructor(private service:AdminService,private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  save(){
+    this.service.addAdminBus(this.addBus);
+    console.log("Saving...." + this.addBus.busNo);
+    alert("Your entry is saved successfully");
+    this.router.navigate(['admin_bus-list']);
+  }
+
+  //logout function
+  logout(){
+    this.router.navigate(['home']);
   }
 
 }
