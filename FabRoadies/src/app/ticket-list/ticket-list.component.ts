3cd0c3020c04ac6862ca227addb5944f1628abe8
BusBookingApp123 <aninditasengupta2@gmail.com>
2021-06-15 14:13:18 +0530

    Update ticket-list.component.ts

--- FabRoadies/src/app/ticket-list/ticket-list.component.ts
+++ FabRoadies/src/app/ticket-list/ticket-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Ticket } from 'src/Ticket.model';
+import { AdminService } from '../services/admin.service';
 
 @Component({
   selector: 'app-ticket-list',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketListComponent implements OnInit {
 
-  constructor() { }
+  tList:Ticket[]=[];
+
+  constructor(private service:AdminService,private router:Router) { }
 
   ngOnInit(): void {
+    // this.service.getAdminBusList().then(data => this.tList = data);
+  }
+
+//   remove(ticketId:string){
+//     var ans = confirm("Are you sure to delete?");
+//     if(ans){
+//     this.service.deleteBus(ticketId);
+//     this.tList = this.tList.filter(tList => tList.ticketId != ticketId);
+//   }
+// }
+
+  //logout
+  logout(){
+    this.router.navigate(['user-home']);
   }
 
 }
