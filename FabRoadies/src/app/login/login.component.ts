e71365d411db793b7a0c42c966d7121cf8544662
Sancharisaha18 <sancharisaha6@gmail.com>
2021-06-15 09:08:57 +0530

    Update login.component.ts

--- FabRoadies/src/app/login/login.component.ts
+++ FabRoadies/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/user.model';
+import { Role } from 'src/role.model';
+import { AdminService } from '../services/admin.service';
 
 @Component({
   selector: 'app-login',
@@ -8,22 +10,30 @@ import { User } from 'src/user.model';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  role: Role = new Role();
   user: User = new User();
   flag : boolean = false;
 
-  constructor(private router:Router) { }
+  constructor(private service : AdminService,private router:Router) { }
 
   ngOnInit(): void {}
 
   validate() {
-    if(this.user.email=='admin@gmail.com'&&this.user.password=='admin123')
+    this.service.loginUser(this.user.email,this.user.password).subscribe(data => this.role = data);
+    // alert(this.role.roleId);
+    if(this.role.roleId===1)
       this.router.navigate(['admin-browse']);
-    else if(this.user.email=='fabroadies@gmail.com' && this.user.password == 'fabroadies')
+    else if(this.role.roleId===2)
       this.flag = true;
     else
       alert("Invalid user id");
-    console.log(this.user.email+":"+this.user.password);
+    // if(this.user.email=='admin@gmail.com'&&this.user.password=='admin123')
+    //   this.router.navigate(['admin-browse']);
+    // else if(this.user.email=='fabroadies@gmail.com' && this.user.password == 'fabroadies')
+    //   this.flag = true;
+    // else
+    //   alert("Invalid user id");
+    // console.log(this.user.email+":"+this.user.password);
   }
 
 }
